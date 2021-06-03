0b8ba7e8386625c3fca215cc36e4b2fd9ca2da2a
Jisse Reitsma <jisse@yireo.com>
2021-06-03 09:43:51 +0200

    Only reset autoloader when file exists

--- src/Listener/ResponseListener.php
+++ src/Listener/ResponseListener.php
@@ -4,7 +4,6 @@ namespace Frosh\HtmlMinify\Listener;
 
 use Composer\Autoload\ClassLoader;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use JSMin\JSMin;
@@ -19,6 +18,8 @@ class ResponseListener
      */
     public function onKernelResponse(ResponseEvent $event): void
     {
+        $start = microtime(true);
+
         if (!$event->isMasterRequest()) {
             return;
         }
@@ -34,40 +35,44 @@ class ResponseListener
             return;
         }
 
+        $this->resetClassLoader();
+
+        $content = $response->getContent();
+        $lengthInitialContent = mb_strlen($content, 'utf8');
+
+        $javascripts = $this->getCombinedInlineScripts($content);
+
+        $this->minifyJavascript($javascripts);
+        $this->minifyHtml($content);
+
+        $content = str_replace($this->javascriptPlaceholder, '<script>' . $javascripts . '</script>', $content);
+
+        $lengthContent = mb_strlen($content, 'utf8');
+        $savedData = round(100 - 100 / ($lengthInitialContent / $lengthContent), 2);
+        $timeTook = (int) ((microtime(true) - $start) * 1000);
+
+        $response->headers->add(['X-Html-Compressor' => time() . ': ' . $savedData . '% ' . $timeTook. 'ms']);
+
+        $response->setContent($content);
+    }
+
+    private function resetClassLoader() {
         $file = __DIR__.'/../../vendor/autoload.php';
+        if (!file_exists($file)) {
+            return;
+        }
+
         $classLoader = require_once $file;
 
         if ($classLoader instanceof ClassLoader) {
             $classLoader->unregister();
             $classLoader->register(false);
         }
-
-        $this->minify($response);
     }
 
-    private function minify(Response $response): void
-    {
-        $startTime = microtime(true);
-        $content = $response->getContent();
-        $lengthInitialContent = mb_strlen($content, 'utf8');
-
-        $this->minifySourceTypes($content);
-
-        $javascripts = $this->extractCombinedInlineScripts($content);
-
-        $this->minifyHtml($content);
-
-        //add the minified javascript after minifying html
-        $content = str_replace($this->javascriptPlaceholder, '<script>' . $javascripts . '</script>', $content);
-
-        $this->assignCompressionHeader($response, $content, $lengthInitialContent, $startTime);
-
-        $response->setContent($content);
-    }
-
-    private function minifyJavascript(string $content): string {
+    private function minifyJavascript(string &$content): void {
         $jsMin = new JSMin($content);
-        return $jsMin->min();
+        $content = $jsMin->min();
     }
 
     private function minifyHtml(string &$content): void {
@@ -77,77 +82,39 @@ class ResponseListener
             '/\n/',
             '/\<\!--.*?-->/',
             '/(\x20+|\t)/', # Delete multispace (Without \n)
-            '/span\>\s+/', # keep whitespace after span tags
-            '/\s+\<span/', # keep whitespace before span tags
-            '/button\>\s+/', # keep whitespace after span tags
-            '/\s+\<button/', # keep whitespace before span tags
+            '/span\>\s+\</', # keep whitespace at span tags
             '/\>\s+\</', # strip whitespaces between tags
             '/(\"|\')\s+\>/', # strip whitespaces between quotation ("') and end tags
-            '/=\s+(\"|\')/', # strip whitespaces between = "'
-            '/' . $this->spacePlaceholder . '/', # replace the spacePlaceholder at the end
-        ];
+            '/=\s+(\"|\')/']; # strip whitespaces between = "'
 
         $replace = [
             "\n",
             "\n",
-            ' ',
-            '',
-            ' ',
-            'span>' . $this->spacePlaceholder,
-            $this->spacePlaceholder . '<span',
-            'button>' . $this->spacePlaceholder,
-            $this->spacePlaceholder . '<button',
-            '><',
-            '$1>',
-            '=$1',
-            ' ',
-        ];
+            " ",
+            "",
+            " ",
+            'span>' . $this->spacePlaceholder . '<',
+            "><",
+            "$1>",
+            "=$1"];
 
         $content = trim(preg_replace($search, $replace, $content));
+        $content = preg_replace('/' . $this->spacePlaceholder . '/', ' ', $content);
     }
 
-    private function extractCombinedInlineScripts(string &$content): string
+    private function getCombinedInlineScripts(string &$content): string
     {
         $scriptContents = '';
         $index = 0;
         $placeholder = $this->javascriptPlaceholder;
         if (strpos($content, '</script>') !== false) {
-            $content = preg_replace_callback('#<script>(.*?)<\/script>#s', function ($matches) use (&$scriptContents, &$index, $placeholder) {
+            $content = preg_replace_callback('#<script>(.*?)<\/script>#s', static function ($matches) use (&$scriptContents, &$index, $placeholder) {
                 $index++;
-                $content = trim($matches[1]);
-
-                if (!$this->str_ends_with($content, ';')) {
-                    $content .= ';';
-                }
-
-                $scriptContents .= $content . PHP_EOL;
+                $scriptContents .= $matches[1] . PHP_EOL;
                 return $index === 1 ? $placeholder : '';
             }, $content);
         }
 
-        return $this->minifyJavascript($scriptContents);
-    }
-
-    private function minifySourceTypes(&$content): void
-    {
-        $search = [
-            '/ type=["\']text\/javascript["\']/',
-            '/ type=["\']text\/css["\']/',
-        ];
-        $replace = '';
-        $content = preg_replace($search, $replace, $content);
-    }
-
-    private function assignCompressionHeader(Response $response, string $content, int $lengthInitialContent, float $startTime): void
-    {
-        $lengthContent = mb_strlen($content, 'utf8');
-        $savedData = round(100 - 100 / ($lengthInitialContent / $lengthContent), 2);
-        $timeTook = (int)((microtime(true) - $startTime) * 1000);
-
-        $response->headers->add(['X-Html-Compressor' => time() . ': ' . $savedData . '% ' . $timeTook . 'ms']);
-    }
-
-    private function str_ends_with(string $haystack, string $needle): bool {
-        return $needle === '' || substr_compare($haystack, $needle, -strlen($needle)) === 0;
+        return $scriptContents;
     }
 }
